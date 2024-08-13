f1472af7d3d2a11b69ae7fcc55e462aed05d69eb
Ignas <ignas.petrausk@gmail.com>
2024-08-14 00:16:52 +0300

    gallery1

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -9,7 +9,7 @@ export default function Gallery() {
             <NavBar />
             <section className="w-full flex flex-wrap justify-center gap-3 mt-8 lg:mt-28">
                 <div className="w-11/12 h-[30vh] lg:w-[30%] overflow-hidden">
-                    <Link to="#" className=""><div className="bg-[url('./images/carousel/lg1.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Vestuvės</p></div></Link>
+                    <Link to="#" className=""><div className="bg-[url(' + require('./images/carousel/lg1.jpg') + ')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Vestuvės</p></div></Link>
                 </div>
                 <div className="w-11/12 h-[30vh] lg:w-[60%] overflow-hidden">
                     <Link to="#" className=""><div className="bg-[url('./images/carousel/lg2.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Krikštynos</p></div></Link>
