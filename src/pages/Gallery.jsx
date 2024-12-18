920d46e001e90048067cfec9e28a7dbd3defc481
Ignas <ignas.petrausk@gmail.com>
2024-12-18 21:21:46 +0200

    updated gallery images

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -9,22 +9,22 @@ export default function Gallery() {
             <NavBar />
             <section className="w-full flex flex-wrap justify-center gap-3 mt-8 lg:mt-28">
                 <div className="w-11/12 h-[30vh] lg:w-[30%] overflow-hidden">
-                    <Link to="#" className=""><div className="bg-[url(' + require('../images/carousel/lg1.jpg') + ')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Vestuvės</p></div></Link>
+                    <Link to="#" className=""><div className="bg-[url('../images/carousel/lg1.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Vestuvės</p></div></Link>
                 </div>
                 <div className="w-11/12 h-[30vh] lg:w-[60%] overflow-hidden">
                     <Link to="#" className=""><div className="bg-[url('../images/carousel/lg2.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Krikštynos</p></div></Link>
                 </div>
                 <div className="w-11/12 h-[30vh] lg:w-[60%] overflow-hidden">
-                    <Link to="#" className=""><div className="bg-[url('./images/carousel/lg3.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Portretai</p></div></Link>
+                    <Link to="#" className=""><div className="bg-[url('../images/carousel/lg3.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Portretai</p></div></Link>
                 </div>
                 <div className="w-11/12 h-[30vh] lg:w-[30%] overflow-hidden">
-                    <Link to="#" className=""><div className="bg-[url('./images/carousel/lg4.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Porų fotosesijos</p></div></Link>
+                    <Link to="#" className=""><div className="bg-[url('../images/carousel/lg4.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Porų fotosesijos</p></div></Link>
                 </div>
                 <div className="w-11/12 h-[30vh] lg:w-[30%] overflow-hidden">
-                    <Link to="#" className=""><div className="bg-[url('./images/carousel/lg5.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Šeimos</p></div></Link>
+                    <Link to="#" className=""><div className="bg-[url('../images/carousel/lg5.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Šeimos</p></div></Link>
                 </div>
                 <div className="w-11/12 h-[30vh] lg:w-[60%] overflow-hidden">
-                    <Link to="#" className=""><div className="bg-[url('./images/carousel/lg6.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Renginiai</p></div></Link>
+                    <Link to="#" className=""><div className="bg-[url('../images/carousel/lg6.jpg')] bg-cover saturate-0 bg-no-repeat h-full bg-center flex justify-center items-center hover:saturate-100 hover:scale-150 transition-all duration-700"><p className="text-4xl font-extrabold opacity-80">Renginiai</p></div></Link>
                 </div>
             </section>
             <Footer />
